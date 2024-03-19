edde61a1cd1a3bc43e161c3f6e58d3a2ab5fc0bb
AaronQA <80923841+AaronQA@users.noreply.github.com>
2024-03-19 15:37:16 +0300

    Update CukesRunner.java

--- src/test/java/com/CRM/runners/CukesRunner.java
+++ src/test/java/com/CRM/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/CRM/step_definitions",
         dryRun = true,
-        tags = "@Wip",
+        tags = "@wip",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
