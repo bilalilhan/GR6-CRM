dcabbc695f32772f69106d63f2e386f6c3bcaf80
Rubinah nikzad <moqads.nikzad2023@gmail.com>
2024-03-18 18:54:16 -0700

    US9_set up

--- src/test/java/com/CRM/pages/US9_ActivityPage.java
+++ src/test/java/com/CRM/pages/US9_ActivityPage.java
@@ -10,16 +10,8 @@ public class US9_ActivityPage {
     public US9_ActivityPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-    @FindBy(name = "Message")
-    public WebElement inputMessage;
 
-    @FindBy(name ="Task")
-    public WebElement InputTask;
-    @FindBy(name ="Event")
-    public WebElement InputEvent;
-    @FindBy(name ="Poll")
-    public WebElement InputPoll;
     @FindBy(name ="More")
-    public WebElement InputMore;
+    public WebElement options;
 
 }
--- src/test/java/com/CRM/step_definitions/US9_ActivityStream.java
+++ src/test/java/com/CRM/step_definitions/US9_ActivityStream.java
@@ -1,13 +1,14 @@
 package com.CRM.step_definitions;
 
 
+import com.CRM.pages.US9_ActivityPage;
 import com.CRM.utilities.BrowserUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class US9_ActivityStream {
-    US9_ActivityStream activityStream = new US9_ActivityStream();
+    US9_ActivityPage activityStream = new US9_ActivityPage();
     @Given("user should be able to see all the options on the Activity stream page")
     public void user_should_be_able_to_see_all_the_options_on_the_activity_stream_page() {
      System.out.println("Activity stream page");
@@ -19,7 +20,7 @@ public class US9_ActivityStream {
     }
     @Then("user clicks to the more button")
     public void user_clicks_to_the_more_button() {
-        activityStream.user_clicks_to_the_more_button();
+        activityStream.options.click();
     }
     @Then("user sees the following {string} under the More tab")
     public void user_sees_the_following_under_the_more_tab(String following) {
